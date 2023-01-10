f2699d09569a4b6315766f3a7670b409bd3160f8
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2023-01-10 09:21:04 -0800

    check year type

---
+++ leap.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ *
+ * C program to determine a leap year using logical && and || operator
+ */
+
+int main(void)
+{
+	int year;
+
+	printf("Enter the year you want to check: ");
+	scanf("%d", &year);
+
+	if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	{
+		printf("year you entered is a leap year\n");
+	}
+	else
+	{
+		printf("year you entered is not a leap yea\n");
+	}
+	return(0);
+}
