c6086aa12dcf271c588b26fa6d10f4435582ac36
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2023-01-14 03:30:22 -0800

    sequence

---
+++ loops-in_c/print_N-sequence.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *
+ * C program to print N sequency
+ *
+ * eg 1, 2, 3, 4, 5, 6, 7, 8,9 
+ */
+
+ int main(void)
+ {
+ 	int n, i;
+	
+	i = 1;
+	
+	printf("Enter number of sequency: ");
+	scanf("%d", &n);
+
+	while(i <= n)
+	{
+		printf("%d ,", i);
+		i++;
+	}
+	
+	printf("\b");
+	return(0);
+}
