c1d8da79af47c54a19562d62507dcf37f7c3f0a5
Mwesigye-Nicholas <nicholastoshack12345@gmail.com>
2023-01-14 02:17:52 -0800

    print numbers

---
+++ loops-in_c/print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *
+ * C PROGRAM TO PRINT A DELAYED LOOP
+ *
+ */
+
+int main(void)
+{
+	int i = 0;
+	
+	while(i <= 100)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return(0);
+}
